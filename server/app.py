0981336686c5b01ee677bb3b786acefbcbd85c64
rm0087 <michetti.ray@gmail.com>
2024-08-19 12:13:56 -0400

    models, patch, post, validates

--- server/app.py
+++ server/app.py
@@ -2,13 +2,53 @@
 
 from flask import request
 from config import app, db
-# from models import MODELS GO HERE
+from models import Car, Person, Dealer
 
 @app.get('/')
 def index():
     return "Hello world"
 
-# write your routes here!
+@app.post('/cars')
+def post_car():
+    data = request.json # <<< the body of the request
+
+    try:
+        new_car = Car(name=data['name'], year=data['year'])
+        db.session.add(new_car)
+        db.session.commit()
+
+        return new_car.to_dict(), 201
+    except:
+        return {'message':'OH NOES'}, 400
+    
+@app.patch('/cars/<int:id>')
+def patch_car(id):
+    data = request.json
+    car = Car.query.where(Car.id == id).first()
+
+    if car:
+        try:
+            for key in data:
+                setattr(car, key, data[key])
+            db.session.add(car)
+            db.session.commit()
+
+            return car.to_dict(), 202
+        except:
+            return {'error': 'Invalid data'}, 400
+    else:
+        return{'error': "Dude where's my car?"}, 404
+
+@app.get('/cars')
+def get_cars():
+    data = Car.query.all()
+    try: 
+        return [car.to_dict() for car in data], 200
+    except:
+        return {'mesesage': 'OH NOES'}, 400
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+##NO AUTH/PROXY STUFF
--- server/models.py
+++ server/models.py
@@ -1,5 +1,50 @@
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
+from sqlalchemy_serializer import SerializerMixin
 
-# write your models here!
+class Car(db.Model, SerializerMixin):
+
+    __tablename__ = 'cars_table'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.String)
+    year = db.Column(db.Integer)
+    dealer_id = db.Column(db.Integer, db.ForeignKey('dealers_table.id'))
+    
+    dealer = db.relationship('Dealer', back_populates='cars')
+
+    serialize_rules = ('-dealer.cars',)
+
+    @validates('year')
+    def validate_year(self, key, inc_year):
+        if inc_year in range(1908, 2025):
+            return inc_year
+        else:
+            raise ValueError('Year must be a valid year')
+
+class Person(db.Model, SerializerMixin):
+
+    __tablename__ = 'persons_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    dealers = db.relationship('Dealer',back_populates='person')
+
+    cars = association_proxy('dealers', 'cars')
+
+    serialize_rules = ('-dealers.person',)
+
+class Dealer(db.Model, SerializerMixin):
+
+    __tablename__ = 'dealers_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    person_id = db.Column(db.Integer, db.ForeignKey('persons_table.id'))
+    
+    person = db.relationship('Person', back_populates='dealers')
+    cars = db.relationship('Car', back_populates='dealer')
+
+    serialize_rules = ('-person.dealers', '-cars.dealer')
